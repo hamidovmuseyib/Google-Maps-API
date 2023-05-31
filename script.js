bc73b6ccbb4e0c22ba13e2473fafcfd2a3212ea4
hamidovmuseyib <hamidov.museyib@gmail.com>
2023-05-31 23:18:07 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let map, directionsService, directionsRenderer;
+let sourceAutocomplete, destAutocomplete;
+let distanceDiv;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.7749, lng: -122.4149},
+        zoom: 13
+            });
+    google.maps.event.addListener(map, "click", function(event) {
+        this.setOptions({scrollwheel: true});
+    });
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+    sourceAutocomplete = new google.maps.places.Autocomplete(document.getElementById('source'));
+    destAutocomplete = new google.maps.places.Autocomplete(document.getElementById('dest'));
+    distanceDiv = document.getElementById('distance');
+}
+
+function calcRoute() {
+    var source = document.getElementById('source').value;
+    var dest = document.getElementById('dest').value;
+
+    let request = {
+        origin: source,
+        destination: dest,
+        travelMode: 'DRIVING',   
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == "OK") {
+        directionsRenderer.setDirections(result);
+        let distance = result.routes[0].legs[0].distance.text.replace("mil", "km");
+        let duration = result.routes[0].legs[0].duration.text.replace("dakika", "dəqiqə").replace("hours", "saat");
+        distanceDiv.innerHTML = "Təxmini məsafə: " + distance + "<br>Təxmini vaxt: " + duration;
+    } else {
+        distanceDiv.innerHTML = "Xəta: Məsafəni tapmaq mümkün olmadı.";
+    }
+}); 
+}
